d273a9441d6d853b6d6a5303774262ea3db8bb07
Marek Šlenker <55733454+MarekSlenker@users.noreply.github.com>
2024-08-06 13:33:16 +0200

    Update Phasing.extract_phase_bcftools_MK.sh

--- Slenker_et_al_2024_Molecular_Ecology/Phasing.extract_phase_bcftools_MK.sh
+++ Slenker_et_al_2024_Molecular_Ecology/Phasing.extract_phase_bcftools_MK.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
+
+#Script to prepare phased haplotype sequences for each for one sample.
+
+prefix=$1
+PLOIDY=$2
+genelist=$3
+
+#Run bcftools to extract sequences
+
+
+for((i=1;i<=$PLOIDY;i++)); do
+    while read GEN; do
+    samtools faidx "$prefix".fasta $GEN | bcftools consensus -H $i ./"$prefix".fasta.snps.whatshap.POLYPHASE.vcf.gz >> ./"$prefix".v"$i".phased.fasta; done < ./"$genelist"
+done
+
+exit
+
 
