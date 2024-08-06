8f8185451444dba0aaaca9dbdb24acd1579b9b13
Marek Šlenker <55733454+MarekSlenker@users.noreply.github.com>
2024-08-06 13:31:00 +0200

    Update Phasing.phase_alleles_with_WhatsHap_MK.sh

--- Slenker_et_al_2024_Molecular_Ecology/Phasing.phase_alleles_with_WhatsHap_MK.sh
+++ Slenker_et_al_2024_Molecular_Ecology/Phasing.phase_alleles_with_WhatsHap_MK.sh
@@ -1,1 +1,30 @@
+#!/bin/bash
+
+
+prefix=$1
+PLD=$2
+
+
+echo "POLYPHASE:"
+echo
+
+pip install whatshap --user --upgrade
+
+python -m whatshap polyphase \
+$prefix.fasta.snps.vcf \
+$prefix.fasta.marked.bam \
+--reference=$prefix.fasta \
+--output $prefix.fasta.snps.POLYPHASE.whatshap.vcf \
+--ploidy $PLD
+
+python -m whatshap stats $prefix.fasta.snps.POLYPHASE.whatshap.vcf \
+--gtf $prefix.whatshap.gtf \
+--tsv $prefix.whatshap.stats.tsv
+
+bgzip -c $prefix.fasta.snps.POLYPHASE.whatshap.vcf > $prefix.fasta.snps.whatshap.POLYPHASE.vcf.gz
+tabix $prefix.fasta.snps.whatshap.POLYPHASE.vcf.gz
+
+
+
+exit
 
