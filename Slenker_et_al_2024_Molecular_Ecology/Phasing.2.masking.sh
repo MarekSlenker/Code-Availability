1ce8e317bea401dc031fb90d8532d506a24f46bd
Marek Šlenker <55733454+MarekSlenker@users.noreply.github.com>
2024-08-06 15:09:46 +0200

    Update Phasing.2.masking.sh

--- Slenker_et_al_2024_Molecular_Ecology/Phasing.2.masking.sh
+++ Slenker_et_al_2024_Molecular_Ecology/Phasing.2.masking.sh
@@ -29,130 +29,19 @@ done
 
 
 
-# while read SAMPLE; do
-#     read PLOIDY
-#     echo "processing $SAMPLE"
 
-#     grep -A 1 --no-group-separator "_h1" "$SAMPLE".phased.haplonerate.iupac.1.fasta | sed 's/supercontig//; s/_h1//' > "$SAMPLE".phased.haplonerate.iupac.h1.fasta
-#     grep -A 1 --no-group-separator "_h2" "$SAMPLE".phased.haplonerate.iupac.1.fasta | sed 's/supercontig//; s/_h2//' > "$SAMPLE".phased.haplonerate.iupac.h2.fasta
-
-#     grep -A 1 --no-group-separator "_h1" "$SAMPLE".phased.haplonerate.iupac.3.fasta | sed 's/supercontig//; s/_h1//' > "$SAMPLE".phased.haplonerate.iupac.h3.fasta
-#     grep -A 1 --no-group-separator "_h2" "$SAMPLE".phased.haplonerate.iupac.3.fasta | sed 's/supercontig//; s/_h2//' > "$SAMPLE".phased.haplonerate.iupac.h4.fasta
-
-#     grep -A 1 --no-group-separator "_h1" "$SAMPLE".phased.haplonerate.iupac.5.fasta | sed 's/supercontig//; s/_h1//' > "$SAMPLE".phased.haplonerate.iupac.h5.fasta
-#     grep -A 1 --no-group-separator "_h2" "$SAMPLE".phased.haplonerate.iupac.5.fasta | sed 's/supercontig//; s/_h2//' > "$SAMPLE".phased.haplonerate.iupac.h6.fasta
-#     ###
-#     grep -A 1 --no-group-separator "_h1" "$SAMPLE".phased.haplonerate.Nmasked.1.fasta | sed 's/supercontig//; s/_h1//' > "$SAMPLE".phased.haplonerate.Nmasked.h1.fasta
-#     grep -A 1 --no-group-separator "_h2" "$SAMPLE".phased.haplonerate.Nmasked.1.fasta | sed 's/supercontig//; s/_h2//' > "$SAMPLE".phased.haplonerate.Nmasked.h2.fasta
-
-#     grep -A 1 --no-group-separator "_h1" "$SAMPLE".phased.haplonerate.Nmasked.3.fasta | sed 's/supercontig//; s/_h1//' > "$SAMPLE".phased.haplonerate.Nmasked.h3.fasta
-#     grep -A 1 --no-group-separator "_h2" "$SAMPLE".phased.haplonerate.Nmasked.3.fasta | sed 's/supercontig//; s/_h2//' > "$SAMPLE".phased.haplonerate.Nmasked.h4.fasta
-
-#     grep -A 1 --no-group-separator "_h1" "$SAMPLE".phased.haplonerate.Nmasked.5.fasta | sed 's/supercontig//; s/_h1//' > "$SAMPLE".phased.haplonerate.Nmasked.h5.fasta
-#     grep -A 1 --no-group-separator "_h2" "$SAMPLE".phased.haplonerate.Nmasked.5.fasta | sed 's/supercontig//; s/_h2//' > "$SAMPLE".phased.haplonerate.Nmasked.h6.fasta
-# done < barbaroides_samplePloidyList_bezOutgroups.txt
-
-
-while read SAMPLE; do
-    read PLOIDY
-    echo "processing $SAMPLE"
-    
-    # grep -A 1 --no-group-separator "_h1" "$SAMPLE".phased.haplonerate.iupac.1.fasta | sed 's/_h1//' > "$SAMPLE".phased.haplonerate.iupac.h1.fasta
-    # grep -A 1 --no-group-separator "_h2" "$SAMPLE".phased.haplonerate.iupac.1.fasta | sed 's/_h2//' > "$SAMPLE".phased.haplonerate.iupac.h2.fasta
-    
-    # grep -A 1 --no-group-separator "_h1" "$SAMPLE".phased.haplonerate.iupac.3.fasta | sed 's/_h1//' > "$SAMPLE".phased.haplonerate.iupac.h3.fasta
-    # grep -A 1 --no-group-separator "_h2" "$SAMPLE".phased.haplonerate.iupac.3.fasta | sed 's/_h2//' > "$SAMPLE".phased.haplonerate.iupac.h4.fasta
-    
-    # grep -A 1 --no-group-separator "_h1" "$SAMPLE".phased.haplonerate.iupac.5.fasta | sed 's/_h1//' > "$SAMPLE".phased.haplonerate.iupac.h5.fasta
-    # grep -A 1 --no-group-separator "_h2" "$SAMPLE".phased.haplonerate.iupac.5.fasta | sed 's/_h2//' > "$SAMPLE".phased.haplonerate.iupac.h6.fasta
-    ###
-    # grep -A 1 --no-group-separator "_h1" "$SAMPLE".phased.haplonerate.Nmasked.1.fasta | sed 's/_h1//' > "$SAMPLE".phased.haplonerate.Nmasked.h1.fasta
-    # grep -A 1 --no-group-separator "_h2" "$SAMPLE".phased.haplonerate.Nmasked.1.fasta | sed 's/_h2//' > "$SAMPLE".phased.haplonerate.Nmasked.h2.fasta
-    
-    # grep -A 1 --no-group-separator "_h1" "$SAMPLE".phased.haplonerate.Nmasked.3.fasta | sed 's/_h1//' > "$SAMPLE".phased.haplonerate.Nmasked.h3.fasta
-    # grep -A 1 --no-group-separator "_h2" "$SAMPLE".phased.haplonerate.Nmasked.3.fasta | sed 's/_h2//' > "$SAMPLE".phased.haplonerate.Nmasked.h4.fasta
-    
-    # grep -A 1 --no-group-separator "_h1" "$SAMPLE".phased.haplonerate.Nmasked.5.fasta | sed 's/_h1//' > "$SAMPLE".phased.haplonerate.Nmasked.h5.fasta
-    # grep -A 1 --no-group-separator "_h2" "$SAMPLE".phased.haplonerate.Nmasked.5.fasta | sed 's/_h2//' > "$SAMPLE".phased.haplonerate.Nmasked.h6.fasta
-    
-    ###
-    
-    # cat "$SAMPLE".v1.phased.fasta >> "$SAMPLE".phased.chimeric.h1.fasta
-    # cat "$SAMPLE".v2.phased.fasta >> "$SAMPLE".phased.chimeric.h2.fasta
-    # cat "$SAMPLE".v3.phased.fasta >> "$SAMPLE".phased.chimeric.h3.fasta
-    # cat "$SAMPLE".v4.phased.fasta >> "$SAMPLE".phased.chimeric.h4.fasta
-    # cat "$SAMPLE".v5.phased.fasta >> "$SAMPLE".phased.chimeric.h5.fasta
-    # cat "$SAMPLE".v6.phased.fasta >> "$SAMPLE".phased.chimeric.h6.fasta
-
-    
-    # for((i=1;i<=$PLOIDY;i=i+1)); do
-    #     echo "$i"
-    #     cat "$SAMPLE".phased.haplonerate.Nmasked."$PLOIDY".fasta >> "$SAMPLE".phased.haplonerate.Nmasked.fasta
-    # done
-
-done < $SAMPLEPLOIDYLIST
-
-
-
+# merge phased and unPhased sequences
 while read SAMPLE; do
     read PLOIDY
     
     echo "processing $SAMPLE"
-    
-    # cat "$SAMPLE".notPhased.fasta >> "$SAMPLE".phased.haplonerate.iupac.h1.fasta
-    # cat "$SAMPLE".notPhased.fasta >> "$SAMPLE".phased.haplonerate.iupac.h2.fasta
-    # cat "$SAMPLE".notPhased.fasta >> "$SAMPLE".phased.haplonerate.iupac.h3.fasta
-    # cat "$SAMPLE".notPhased.fasta >> "$SAMPLE".phased.haplonerate.iupac.h4.fasta
-    # cat "$SAMPLE".notPhased.fasta >> "$SAMPLE".phased.haplonerate.iupac.h5.fasta
-    # cat "$SAMPLE".notPhased.fasta >> "$SAMPLE".phased.haplonerate.iupac.h6.fasta
     
     for((i=1;i<=$PLOIDY;i=i+1)); do
         echo "$i"
-        cat "$SAMPLE".notPhased.fasta >> "$SAMPLE".phased.haplonerate.Nmasked."$i".fasta
+        cat "$SAMPLE".unPhased.fasta >> "$SAMPLE".phased.haplonerate.Nmasked."$i".fasta
     done
-
-    # cat "$SAMPLE".notPhased.fasta >> "$SAMPLE".phased.haplonerate.Nmasked.h1.fasta
-    # cat "$SAMPLE".notPhased.fasta >> "$SAMPLE".phased.haplonerate.Nmasked.h2.fasta
-    # cat "$SAMPLE".notPhased.fasta >> "$SAMPLE".phased.haplonerate.Nmasked.h3.fasta
-    # cat "$SAMPLE".notPhased.fasta >> "$SAMPLE".phased.haplonerate.Nmasked.h4.fasta
-    # cat "$SAMPLE".notPhased.fasta >> "$SAMPLE".phased.haplonerate.Nmasked.h5.fasta
-    # cat "$SAMPLE".notPhased.fasta >> "$SAMPLE".phased.haplonerate.Nmasked.h6.fasta
-    
-    # cat "$SAMPLE".notPhased.fasta >> "$SAMPLE".phased.chimeric.h1.fasta
-    # cat "$SAMPLE".notPhased.fasta >> "$SAMPLE".phased.chimeric.h2.fasta
-    # cat "$SAMPLE".notPhased.fasta >> "$SAMPLE".phased.chimeric.h3.fasta
-    # cat "$SAMPLE".notPhased.fasta >> "$SAMPLE".phased.chimeric.h4.fasta
-    # cat "$SAMPLE".notPhased.fasta >> "$SAMPLE".phased.chimeric.h5.fasta
-    # cat "$SAMPLE".notPhased.fasta >> "$SAMPLE".phased.chimeric.h6.fasta
     
 done < $SAMPLEPLOIDYLIST
-
-
-
-
-# # AK si pridaval _h1 ... h6 to musi ist prec
-
-# while read SAMPLE; do
-#     read PLOIDY
-
-#     echo "processing $SAMPLE"
-
-#     sed -i 's/_h1/supercontig/' "$SAMPLE".phased.haplonerate.iupac.h1.fasta
-#     sed -i 's/_h2/supercontig/' "$SAMPLE".phased.haplonerate.iupac.h2.fasta
-#     sed -i 's/supercontig_h3/supercontig/' "$SAMPLE".phased.haplonerate.iupac.h3.fasta
-#     sed -i 's/supercontig_h4/supercontig/' "$SAMPLE".phased.haplonerate.iupac.h4.fasta
-#     sed -i 's/supercontig_h5/supercontig/' "$SAMPLE".phased.haplonerate.iupac.h5.fasta
-#     sed -i 's/supercontig_h6/supercontig/' "$SAMPLE".phased.haplonerate.iupac.h6.fasta
-#     ###
-#     sed -i 's/_h1/supercontig/' "$SAMPLE".phased.haplonerate.Nmasked.h1.fasta
-#     sed -i 's/_h2/supercontig/' "$SAMPLE".phased.haplonerate.Nmasked.h2.fasta
-#     sed -i 's/supercontig_h3/supercontig/' "$SAMPLE".phased.haplonerate.Nmasked.h3.fasta
-#     sed -i 's/supercontig_h4/supercontig/' "$SAMPLE".phased.haplonerate.Nmasked.h4.fasta
-#     sed -i 's/supercontig_h5/supercontig/' "$SAMPLE".phased.haplonerate.Nmasked.h5.fasta
-#     sed -i 's/supercontig_h6/supercontig/' "$SAMPLE".phased.haplonerate.Nmasked.h6.fasta
-
-# done < barbaroides_samplePloidyList_bezOutgroups.txt
-
 
 
 
@@ -165,7 +54,6 @@ read SAMPLE1 < $SAMPLEPLOIDYLIST
 fastaLength=$(grep -c ">" "$SAMPLE1".phased.haplonerate.Nmasked.1.fasta)
 doubleFastaLength=$((fastaLength+fastaLength))
 echo $doubleFastaLength
-# grep ">" "$SAMPLE1".v1.vsetky.fasta | sed s'/>//' > genes.txt
 
 mkdir RESDIR
 
@@ -173,9 +61,7 @@ mkdir RESDIR
 
 while read SAMPLE; do
     read PLOIDY
-    
-    # viem ze mam PLOIDY suborov
-    
+
     for((pld=1;pld<=$PLOIDY;pld++)); do  # pre kazdy jeden variant = ploidia
         echo "processing $SAMPLE, pld $pld/$PLOIDY"
         
@@ -183,17 +69,11 @@ while read SAMPLE; do
             
             exonName=$(sed "${line}q;d" "$SAMPLE".phased.haplonerate.Nmasked."$pld".fasta | sed 's/>//' | sed 's/ //')
             
-            # echo $(sed "${line}q;d" "$SAMPLE".v"$pld".vsetky.fasta)_h"$pld" >> ./RESDIR/"$exonName".fasta
-            
-            # echo ">$SAMPLE-h$pld" >> ./RESDIR/"$exonName".iupac.fasta
             echo ">$SAMPLE-h$pld" >> ./RESDIR/"$exonName".Nmasked.fasta
-            # echo ">$SAMPLE-h$pld" >> ./RESDIR/"$exonName".chimeric.fasta
             
             let "line++"
             
-            # echo $(sed "${line}q;d" "$SAMPLE".phased.haplonerate.iupac.h"$pld".fasta) >> ./RESDIR/"$exonName".iupac.fasta
             echo $(sed "${line}q;d" "$SAMPLE".phased.haplonerate.Nmasked."$pld".fasta) >> ./RESDIR/"$exonName".Nmasked.fasta
-            # echo $(sed "${line}q;d" "$SAMPLE".phased.chimeric.h"$pld".fasta) >> ./RESDIR/"$exonName".chimeric.fasta
         done
         
     done
@@ -206,13 +86,13 @@ done < $SAMPLEPLOIDYLIST
 # "Phasing statistics"
 
 
-printf "Individual\tfazovane\tNEfazovane\n" > Phasing_statistics.txt
+printf "Individual\tphased\tunPhased\n" > Phasing_statistics.txt
 while read SAMPLE; do
     read PLOIDY
     
     { printf '%s\t' "$SAMPLE"
         echo "$(($(grep -c ">" "$SAMPLE".phased.haplonerate.Nmasked.1.fasta)/2))" | xargs printf '%s\t%s'
-        echo "$(grep -c ">" "$SAMPLE".notPhased.fasta)" | xargs printf '%s\t\n%s'
+        echo "$(grep -c ">" "$SAMPLE".unPhased.fasta)" | xargs printf '%s\t\n%s'
                 
     } >> Phasing_statistics.txt    
 done < $SAMPLEPLOIDYLIST
