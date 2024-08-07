4c533e51b323714c8207b5f3ba8fb92db7728ce4
Marek Šlenker <55733454+MarekSlenker@users.noreply.github.com>
2024-08-07 12:05:07 +0200

    Update ML_Trees.concat.1.bestFittingModels.sh

--- Slenker_et_al_2024_Molecular_Ecology/ML_Trees.concat.1.bestFittingModels.sh
+++ Slenker_et_al_2024_Molecular_Ecology/ML_Trees.concat.1.bestFittingModels.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-export FASTA       # fasta file with concatenated sequences
+export ALIGNMENT       # fasta file with concatenated sequences
 export PARTITIONS  # partition of the fasta file
 export PBS_NUM_PPN # number of cores
 
@@ -12,11 +12,11 @@ module add iqtree/1.6.12
 cd "$SCRATCHDIR"
 
 
-cp $FASTA .
+cp $ALIGNMENT .
 cp $PARTITIONS .
 
 
-iqtree -s $FASTA -spp $PARTITIONS -m TESTMERGEONLY -st DNA -nt $PBS_NUM_PPN -pre iqtreeoutput -cptime 3600 -rclusterf 10 -safe >>iqtree.output.log 2>&1
+iqtree -s $ALIGNMENT -spp $PARTITIONS -m TESTMERGEONLY -st DNA -nt $PBS_NUM_PPN -pre iqtreeoutput -cptime 3600 -rclusterf 10 -safe >>iqtree.output.log 2>&1
 
 
 exit
