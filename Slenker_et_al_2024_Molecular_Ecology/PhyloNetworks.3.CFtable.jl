0cd5d2632c0e31b813312af70c51cecbe8a4c4d9
Marek Šlenker <55733454+MarekSlenker@users.noreply.github.com>
2024-08-08 10:11:29 +0200

    Create PhyloNetworks.3.CFtable.jl

---
+++ Slenker_et_al_2024_Molecular_Ecology/PhyloNetworks.3.CFtable.jl
@@ -0,0 +1,20 @@
+#!/usr/bin/env julia
+
+
+using PhyloNetworks
+using CSV
+using DataFrames
+using PhyloPlots
+using RCall
+
+
+
+besttrees = readMultiTopology("besttrees.tre");
+q,t = countquartetsintrees(besttrees);
+individualsCF = writeTableCF(q,t)
+CSV.write("tableCF.astral.individuals.csv", individualsCF); # to save the data frame to a file
+
+mapAllelesCFtable("allele-species-map.csv", "tableCF.astral.individuals.csv"; filename = "tableCF.astral.speciesNames.csv")
+
+
+
