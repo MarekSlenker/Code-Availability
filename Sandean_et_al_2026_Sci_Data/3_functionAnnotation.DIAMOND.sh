15bb0a5f63f4b352d54dcddaf6261e1ad3961e38
Marek Šlenker <55733454+MarekSlenker@users.noreply.github.com>
2026-02-02 22:22:21 +0100

    Update 3_functionAnnotation.DIAMOND.sh

--- Sandean_et_al_2026_Sci_Data/3_functionAnnotation.DIAMOND.sh
+++ Sandean_et_al_2026_Sci_Data/3_functionAnnotation.DIAMOND.sh
@@ -10,7 +10,7 @@
 cd <PATH>/<PATH>/function_annotation/refseq
 <PATH>/bin/diamond/diamond blastp \
 -d <PATH>/AnnotationDTBSs/RefSeq/refseq \
--q ../../C087_203_hap1.EVM.pep \
+-q ../../C087_203_hap1.PASA.longest_isoform.aa \
 --evalue 0.00001 \
 --outfmt 6 qseqid qlen qstart qend sseqid slen sstart send pident ppos length score evalue stitle \
 --salltitles --max-target-seqs 1 \
@@ -20,7 +20,7 @@ cd <PATH>/<PATH>/function_annotation/refseq
 cd <PATH>/<PATH>/function_annotation/swissprot
 <PATH>/bin/diamond/diamond blastp \
 -d <PATH>/AnnotationDTBSs/Swiss-Prot/swissprot \
--q ../../C087_203_hap1.EVM.pep \
+-q ../../C087_203_hap1.PASA.longest_isoform.aa \
 --evalue 0.00001 \
 --outfmt 6 qseqid qlen qstart qend sseqid slen sstart send pident ppos length score evalue stitle \
 --max-target-seqs 1 \
@@ -31,7 +31,7 @@ cd <PATH>/<PATH>/function_annotation/swissprot
 cd <PATH>/<PATH>/function_annotation/TrEMBL
 <PATH>/bin/diamond/diamond blastp \
 -d <PATH>/AnnotationDTBSs/TrEMBL/trembl \
--q ../../C087_203_hap1.EVM.pep \
+-q ../../C087_203_hap1.PASA.longest_isoform.aa \
 --evalue 0.00001 \
 --outfmt 6 qseqid qlen qstart qend sseqid slen sstart send pident ppos length score evalue stitle \
 --max-target-seqs 1 \
